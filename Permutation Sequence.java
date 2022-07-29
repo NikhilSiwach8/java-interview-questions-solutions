2fb93ccc1e5e74e0321181b7f340f323f53a69b9
Nikhil Siwach <100553617+NikhilSiwach8@users.noreply.github.com>
2022-07-29 09:13:56 +0530

    Java Solution - LeetCode

---
+++ Permutation Sequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        List<Integer> nums = new ArrayList<>();
+        int[] factorial = new int[n+1];
+        
+        for(int i = 1; i <= n; ++i)
+            nums.add(i);
+        
+        Arrays.fill(factorial, 1);
+        for(int i = 2; i <= n; ++i)
+            factorial[i] = factorial[i-1]*i;
+        --k;
+        
+        for(int i = n-1; i >= 0; --i){
+            final int j = k / factorial[i];
+            k %= factorial[i];
+            sb.append(nums.get(j));
+            nums.remove(j);
+        }
+        return sb.toString();
+    }
+}
